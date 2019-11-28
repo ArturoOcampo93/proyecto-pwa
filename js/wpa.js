952cebcd3018fafcdc7b48e9fff69a97ac4a521a
ArturoOcampo93 <arturo.ocampo93@gmail.com>
2019-11-28 13:56:55 -0600

    PWA PRUEBA

--- js/wpa.js
+++
@@ -1,9 +0,0 @@
-if('serviceWorker' in navigator){
-    console.log('Service Worker esta activo');
-    navigator.serviceWorker.register('./js/sw.js')
-        .then(res => console.log('Service Worker cargado correctamente',res))
-        .catch(err => console.log('Service Worker no se ha podido registrar',err))
-}
-else{
-    console.log('Service Worker esta inactivo');
-}
